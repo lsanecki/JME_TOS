dfff24ccc30d07acae7dccd66b496b8d13e7ad00
Łukasz Sanecki <lsanecki@jm.pl>
2023-02-20 14:14:34 +0100

    Dodano wywoływanie funkcji dla calego urządzenia

---
+++ Liblary/deviceFun.py
@@ -0,0 +1,10 @@
+def fun_a(param):
+    print('fun_a: {}'.format(param))
+
+
+def fun_b(param):
+    print('fun_b: {}'.format(param))
+
+
+def fun_c(param):
+    print('fun_c: {}'.format(param))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Liblary.libJson import *
-
+from Liblary.testStep import *
+import Liblary.deviceFun
 
 def set_option(param):
     if param == 1:
@@ -16,19 +17,40 @@ def run_test(test_steps):
 
 
 def run_step(nr_step, test_steps):
+    step_test = TestStep()
+
     step = next((sub for sub in test_steps if sub['StepNr'] == nr_step), None)
-    step_title = step['Title']
-    step_description = step['Description']
-    step_show_message = step['ShowMsg']
-    step_delay = step['Delay']
-    step_set_flag = set_option(step['SetFlag'])
-    step_one_test_all_module = set_option(step['OneTestForAllModule'])
-    step_fun_for_all_module = step['Function']
-    step_next_nr = step['NextStep']
-    step_fail_nr = step['FailTestNextStep']
-    step_module = step['Module']
-    print('{}: {} - {}'.format(nr_step, step_title, step_description))
-    return step_next_nr
+    read_step_parameters(nr_step, step, step_test)
+    print('{}: {} - {}'.format(step_test.nr, step_test.title, step_test.description))
+    print(step_test.show_message)
+    if step_test.one_test_all_module:
+        for function in step_test.fun_for_all_module:
+            function_name = function['NameFun']
+            function_parameters = function['Parameters']
+            item = getattr(Liblary.deviceFun, function_name)
+            if callable(item):
+                item(function_parameters)
+
+
+    else:
+        if step_test.flag:
+            pass
+
+    return step_test.next_nr
+
+
+def read_step_parameters(nr_step, step, step_test):
+    step_test.nr = nr_step
+    step_test.title = step['Title']
+    step_test.description = step['Description']
+    step_test.show_message = step['ShowMsg']
+    step_test.delay = step['Delay']
+    step_test.flag = set_option(step['SetFlag'])
+    step_test.one_test_all_module = set_option(step['OneTestForAllModule'])
+    step_test.fun_for_all_module = step['Function']
+    step_test.next_nr = step['NextStep']
+    step_test.fail_nr = step['FailTestNextStep']
+    step_test.module = step['Module']
 
 
 def main():
