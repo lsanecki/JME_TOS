ac7fab37e7611f6ce959372b3b3d55bcac91dcfb
Łukasz Sanecki <lsanecki@jm.pl>
2023-02-20 12:47:44 +0100

    Dodano klase testStep

---
+++ Liblary/testStep.py
@@ -0,0 +1,14 @@
+
+class TestStep:
+    def __init__(self):
+        self.nr = None
+        self.title = None
+        self.description = None
+        self.show_message = None
+        self.delay = None
+        self.flag = None
+        self.one_test_all_module = None
+        self.fun_for_all_module = None
+        self.next_nr = None
+        self.fail_nr = None
+        self.module = None
